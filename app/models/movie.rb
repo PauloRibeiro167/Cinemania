2a652db5f5e117301608eff31fdaed8ef4da9d91
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2024-06-12 10:16:46 -0300

    feat: altera o idioma para pt-br

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,7 +7,7 @@ class Movie
   API_KEY = ENV['TMDB_API_KEY']
 
   def self.list_releases
-    url = "#{BASE_URL}/now_playing?api_key=#{API_KEY}"
+    url = "#{BASE_URL}/now_playing?api_key=#{API_KEY}&language=pt-BR"
     respond = HTTParty.get(url)
     response = JSON.parse(respond.body)
   end
