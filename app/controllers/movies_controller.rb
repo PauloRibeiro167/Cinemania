4c3da4b85048eed870f1fc3b5302bda19ea07282
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2024-06-12 11:51:04 -0300

    feat adiciona o metodo de pesquisa

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,20 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.list_releases
+    results = Movie.list_releases
+    @movies = results
     @movies ||= []
+  end
+
+  def search
+    results = Movie.search_movie(movie_params[:query])
+    @movies = Kaminari.paginate_array(results['results']).page(params[:page]).per(10)
+    @movies ||= []
+    render :result
+  end
+
+  private
+
+  def movie_params
+    params.permit(:query)
   end
 end
